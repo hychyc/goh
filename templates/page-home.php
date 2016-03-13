522d5715f1a4d16a49b2d261d7784a8e0243c982
HYC <haoyingchang@HY.local>
2016-03-12 19:32:33 -0500

    - update favicon - homepage thumbnai

--- templates/page-home.php
+++ templates/page-home.php
@@ -72,7 +72,7 @@
 									</div> -->
 									<div class="col-xs-12 col-md-5 col-md-push-7">
 										<div class="media-box">
-											<img class="media-box__img" src="<?php echo get_template_directory_uri(); ?>/library/images/ph03.png">
+											<img class="media-box__img" src="<?php echo get_template_directory_uri(); ?>/library/images/home-make-a-difference.jpg">
 											<a href="<?php echo get_page_link(21); ?>" class="media-box__title">
 												make a difference
 											</a>
